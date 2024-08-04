fdf53f444126c075072cfe5209c0b22b99161e91
potopaa <156847443+potopaa@users.noreply.github.com>
2024-08-04 23:18:28 +0300

    Add files via upload

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,19 @@
+"""
+Given an integer x, return true if x is a palindrome , and false otherwise.
+"""
+
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+
+        new = 0
+        orig = x
+
+        while x:
+            x, d = divmod(x, 10)
+
+            new = new * 10 + d
