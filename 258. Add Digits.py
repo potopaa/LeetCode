06e9317d658b5e7d11ca88dd0996ddb2a7feec03
potopaa <156847443+potopaa@users.noreply.github.com>
2024-08-06 23:14:53 +0300

    Add files via upload

---
+++ 258. Add Digits.py
@@ -0,0 +1,21 @@
+'''
+
+Given an integer num,
+repeatedly add all its digits until the result has only one digit, and return it.
+
+'''
+
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+
+        while num >= 10:
+
+            cur = num
+
+            new_num = 0
+
+            while cur:
+                cur, d = divmod(cur, 10)
+
+                new_num += d
