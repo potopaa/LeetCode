d3b09533a115a11aa57302acb87f9c9b4fc2a2da
potopaa <156847443+potopaa@users.noreply.github.com>
2024-08-07 23:40:33 +0300

    Add files via upload

---
+++ 145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,29 @@
+'''
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+'''
+
+
+from typing import Optional
+from typing import List
+
+
+class TreeNode:
+    pass
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def helper(node):
+            if node:
+                helper(node.left)
+                helper(node.right)
+                ans.append(node.val)
+
+        helper(root)
+
+        return ans
+    
