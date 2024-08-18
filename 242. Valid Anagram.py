365720ad60d53193e2fd28f45588473c268fd0b6
potopaa <156847443+potopaa@users.noreply.github.com>
2024-08-18 15:02:13 +0300

    Add files via upload

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,14 @@
+'''
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+'''
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
