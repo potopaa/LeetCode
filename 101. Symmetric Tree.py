7d0cc3fe5103b8a2064d9f0c0a202d79d8c3a05a
potopaa <156847443+potopaa@users.noreply.github.com>
2024-08-19 22:58:17 +0300

    Update 101. Symmetric Tree.py

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -29,13 +29,12 @@ Follow up: Could you solve it both recursively and iteratively?
 from typing import Optional
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class TreeNode:
-    pass
+ class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
 
 
 class Solution:
