8c321ccfb67784f4b28f7b25679c9c800020a30d
potopaa <156847443+potopaa@users.noreply.github.com>
2024-08-26 22:53:47 +0300

    Update 1346. Check If N and Its Double Exist.py

--- 1346. Check If N and Its Double Exist.py
+++ 1346. Check If N and Its Double Exist.py
@@ -6,7 +6,6 @@ i != j
 0 <= i, j < arr.length
 arr[i] == 2 * arr[j]
 
-
 Example 1:
 
 Input: arr = [10,2,5,3]
@@ -17,7 +16,6 @@ Example 2:
 Input: arr = [3,1,7,11]
 Output: false
 Explanation: There is no i and j that satisfy the conditions.
-
 
 Constraints:
 
