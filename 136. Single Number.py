7a9ed7f4e385aa97e19f239f6af537d2cdc3769e
potopaa <156847443+potopaa@users.noreply.github.com>
2024-09-19 13:18:44 +0300

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -20,7 +20,7 @@ Constraints:
 -3 * 104 <= nums[i] <= 3 * 104
 Each element in the array appears twice except for one element which appears only once.
 
-'''
+"""
 
 from typing import List
 
