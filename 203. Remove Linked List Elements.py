988410deef9ecf917281f54d1d73072018e9e4b1
potopaa <156847443+potopaa@users.noreply.github.com>
2024-09-20 16:12:19 +0300

    Update 203. Remove Linked List Elements.py

--- 203. Remove Linked List Elements.py
+++ 203. Remove Linked List Elements.py
@@ -1,9 +1,9 @@
-'''
+"""
 
 Given the head of a linked list and an integer val, remove all the nodes of the
 linked list that has Node.val == val, and return the new head.
 
-'''
+"""
 
 from typing import Optional
 
