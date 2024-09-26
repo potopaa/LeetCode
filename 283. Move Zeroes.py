06153c501e869b8de9277138269d2ca352f030fe
potopaa <156847443+potopaa@users.noreply.github.com>
2024-09-26 22:23:02 +0300

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -13,10 +13,7 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-
+     
         j = 0
 
         for i in range(len(nums)):
