1ab77442adbefc1e50232958d0cd306cf2a4957e
potopaa <156847443+potopaa@users.noreply.github.com>
2024-09-26 22:22:01 +0300

    Update 278. First Bad Version.py

--- 278. First Bad Version.py
+++ 278. First Bad Version.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 You are a product manager and currently leading a team to develop a new product.
 Unfortunately, the latest version of your product fails the quality check.
@@ -10,7 +10,7 @@ which causes all the following ones to be bad.
 You are given an API bool isBadVersion(version) which returns whether version is bad.
 Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
-'''
+"""
 
 
 # The isBadVersion API is already defined for you.
