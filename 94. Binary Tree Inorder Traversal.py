596bd9e7e500db96a7d8e762223b84fcd49a8c8d
potopaa <156847443+potopaa@users.noreply.github.com>
2025-07-17 17:40:05 +0300

    Update and rename 94. Binary Tree Inorder Traversal to 94. Binary Tree Inorder Traversal.py

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        ans = []
+        stack = []
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            ans.append(cur.val)
+            cur = cur.right
+        return ans
